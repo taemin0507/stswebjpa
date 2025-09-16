74b6d70a61a11521fe9b81dc3290eaf81f7a5183
taemin0507 <xoals28655@gmail.com>
2025-09-16 12:46:22 +0900

    0916#1

---
+++ spring-restart.sh
@@ -0,0 +1,13 @@
+SPRING_PID=$(pgrep -f stswebjpa-0.0.1-SNAPSHOT.jar)
+SPRING_PATH="home/leetaemin/stswebjpa/target/stswebjpa-0.0.1-SNAPSHOT.jar"
+
+echo $SPRING_PID
+echo $SPRING_PATH
+
+if[ -z "%SPRING_PID" ]; then
+  echo "스프링이 종료된 상태"
+  echo "스프링 재시작 - $(date)" 1>> /home/leetaemin/stswebjpa/spring-restart.log
+  nohup sudo java -jar $SPRING_PATH 1> /home/leetaemin/stswebjpa/log.out 2>  /home/leetaemin/stswebjpa/err.out
+else
+  echo "스프링이 시작된 상태"
+fi
